64d8bc1edf645545600d4958c538e7a96745ace1
abouv <antoine.bouveret@edu.ece.fr>
2025-04-02 13:58:37 +0200

    premier commit test

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//projet debute le 02.04.25 par Antoine Bouveret, Bryan Tachi et Boris Takou.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
